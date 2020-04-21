b59c6a09d311fc6b436cc6bd21c7e74b447c6bcd
Loris Torikian <loris.torikian@slow-loris.fr>
2020-04-21 16:18:44 +0200

    help: done

--- src/minimake.c
+++ src/minimake.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <options/options.h>
 
-#include <stdio.h>
+static void help(void)
+{
+    printf("Usage: minimake [ -f filename ] [ -h ] [ -p ] [ RULES ... ]\n");
+    exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char *argv[])
 {
     struct options opt;
     parse_options(argc, argv, &opt);
-    for (unsigned i = 1 + opt.rule_offset; argv[i] != NULL; ++i)
-        printf("%s\n", argv[i]);
+    if (opt.help)
+        help();
 }
