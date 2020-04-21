a2cbb5d83e3ca5679396582a6c60014d6cad1602
Loris Torikian <loris.torikian@slow-loris.fr>
2020-04-21 18:42:01 +0200

    parse: documented and added parse shell and documented the header

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,3 +1,20 @@
 #include <parse/parse.h>
 
 struct parsed *g_parsed = NULL;
+
+FILE *get_makefile(const char *file)
+{
+    (void) file;
+    return NULL;
+}
+
+void parse(const char *file)
+{
+    (void) file;
+}
+
+void parsed_free(void)
+{
+    linked_free(&g_parsed->variables);
+    linked_free(&g_parsed->rules);
+}
--- src/parse/parse.h
+++ src/parse/parse.h
@@ -1,27 +1,61 @@
+/**
+ * @file parse.h
+ * @author toriki_l
+ * @brief File containing definitions needed for parsing
+ *
+ * This file contains the definition of the `variable' struc, the `rule'
+ * struct, the `parsed' struct, the g_parsed global variables and the parse
+ * function.
+ *
+ */
 #ifndef PARSE_H
 #define PARSE_H
 
+#include <stdio.h>
+
 #include <parse/linked.h>
 
+/**
+ * @brief The struct used to store a variable
+ */
 struct variable
 {
-    char *name;
-    char *value;
+    char *name; /**< The name of the variable */
+    char *value; /**< The value of the variable */
 };
 
+/**
+ * @brief The struct used to store a rule
+ */
 struct rule
 {
-    char *taget;
-    struct linked dependencies;
-    struct linked commands;
+    char *taget; /**< The name of the target */
+    struct linked dependencies; /**< The linked struct containing the list of dependencies */
+    struct linked commands; /**< The linked struct containing the list of commands */
 };
 
+/**
+ * @brief The struct used to store the rules and variables of the file
+ */
 struct parsed
 {
-    struct linked rules;
-    struct linked variables;
+    struct linked rules; /**< The linked struct containing the list of rules */
+    struct linked variables; /**< The linked struct containing the list of variables */
 };
 
+/**
+ * @brief The global variable containing the parsed informations
+ */
 extern struct parsed *g_parsed;
+
+/**
+ * @brief The function that parses the file
+ *
+ * The parsed informations are stored in the g_parsed variable.
+ * If the file was not found, g_parsed is null.
+ *
+ * @param file If null, looks for makefile or Makefile, else the filename
+ */
+void parse(const char *file);
 
 #endif /* PARSE_H */
